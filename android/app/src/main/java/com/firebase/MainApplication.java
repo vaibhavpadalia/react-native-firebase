c5b03978e3e2af5c8ee899c3d79967ca1154585a
Vaibhav Padalia <vaibhav.padalia@kelltontech.com>
2020-04-14 21:01:33 +0530

    Upgrading changes

--- android/app/src/main/java/com/firebase/MainApplication.java
+++ android/app/src/main/java/com/firebase/MainApplication.java
@@ -1,13 +1,14 @@
 package com.firebase;
 
 import android.app.Application;
-
+import android.content.Context;
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import java.util.Arrays;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
