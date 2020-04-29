865d3c38ee83c917a84127ee828bca7ade0af403
Vaibhav Padalia <vaibhav.padalia@kelltontech.com>
2020-04-29 19:58:47 +0530

    Changes in routes

--- src/Routes.js
+++
@@ -1,6 +0,0 @@
-import { createStackNavigator } from "react-navigation";
-import Home from './Home/Home';
-
-export default Routes = createStackNavigator({
-    Home: { screen: Home }
-})
--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
-import Home from './Home/Home';
-
-const Routes = createStackNavigator({
-  Home: { screen: Home }
-});
-
-export default createAppContainer(Routes);
-
